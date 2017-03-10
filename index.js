569b12fbbae036d56a120af662ffb5a54aafe19b
ximing <ximing@meituan.com>
2017-03-10 11:06:43 +0800

    init project

---
+++ index.js
@@ -0,0 +1,4 @@
+/**
+ * Created by yeanzhi on 17/3/10.
+ */
+'use strict';
---
+++ webpack.build.config.js
@@ -0,0 +1,62 @@
+/**
+ * Created by yeanzhi on 17/2/25.
+ */
+'use strict';
+const {
+    resolve
+} = require('path');
+const webpack = require('webpack');
+var node_modules = resolve(__dirname, 'node_modules');
+var pathToReact = resolve(node_modules, 'react/dist/react.min.js');
+
+module.exports = {
+    entry: {
+        'uploader': [
+            './src/index.js'
+        ]
+    },
+    output: {
+        filename: '[name].js',
+        sourceMapFilename: '[file].map',
+        path: resolve(__dirname, 'dist'),
+        publicPath: '/dist',
+        library: 'uploader',
+        libraryTarget: 'umd'
+    },
+    devtool: 'cheap-module-source-map',
+
+    performance: {
+        hints: false
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use:[{
+                    loader: 'babel-loader',
+                    options: {
+                        'presets': [
+                            ['es2015', {
+                                'modules': false
+                            }], 'stage-0'
+                        ],
+                        'env': {},
+                        'ignore': [
+                            'node_modules/**',
+                            'dist'
+                        ],
+                        'plugins': [
+                            'transform-decorators-legacy'
+                        ]
+                    }
+                }],
+                exclude: /node_modules/
+            }
+        ]
+    },
+    externals: [
+    ],
+    plugins: [
+        new webpack.NamedModulesPlugin()
+    ]
+};
